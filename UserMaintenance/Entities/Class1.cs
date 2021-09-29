517486937cf1cdfc1833889d49be9c3cade64400
Sáfrán Viktor <viktor.safran@stud.uni-corvinus.hu>
2021-09-29 18:52:53 +0200

    5. pontig kész

---
+++ UserMaintenance/Entities/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace UserMaintenace.Entities
+{
+    public class User
+    {
+	{
+    public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+    } 
+}
+
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -15,6 +15,19 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
